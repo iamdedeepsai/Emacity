b47f7997a2c8087c5ac7a68d58930780138646c7
iamdedeepsai <dedeepsai.vadapalli@gmail.com>
2022-11-20 22:34:57 +0800

    thats crazy

--- assets/js/preorder.js
+++ assets/js/preorder.js
@@ -1,3 +1,23 @@
-function open() {
-    document.getElementById('popup').style.display = "block";
+function change() {
+    let e = document.getElementById("ret");
+    let text = e.options[e.selectedIndex].text;
+
+    switch (text) {
+        case "AirEm":
+            document.getElementById("image2").style.display = "none";
+            document.getElementById("image1").style.display = "block";
+
+            document.getElementById("model").innerText = "Model: AirEm";
+            document.getElementById("cost").innerText = "Upfront cost: $99";
+            document.getElementById("sub").innerText = "Subscription cost: $19/monthly";
+            break;
+        case "WaterEm":
+            document.getElementById("image1").style.display = "none";
+            document.getElementById("image2").style.display = "block";
+
+            document.getElementById("model").innerText = "Model: WaterEm";
+            document.getElementById("cost").innerText = "Upfront cost: $149";
+            document.getElementById("sub").innerText = "Subscription cost: $29/monthly";
+            break
+    }
 }
